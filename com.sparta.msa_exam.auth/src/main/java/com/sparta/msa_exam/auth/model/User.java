b894bbecf6e1b327ec4c76dcccca90352d5beb0b
strongcookdas <goodjav141592@gmail.com>
2024-12-02 12:38:19 +0900

    chore : user 엔티티 table 어노테이션 추가

--- com.sparta.msa_exam.auth/src/main/java/com/sparta/msa_exam/auth/model/User.java
+++ com.sparta.msa_exam.auth/src/main/java/com/sparta/msa_exam/auth/model/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,6 +17,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "p_user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
