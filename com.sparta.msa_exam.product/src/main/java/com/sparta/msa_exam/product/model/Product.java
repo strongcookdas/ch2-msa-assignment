6b0a28784eb73e164cf625bf652f30c8d9910989
strongcookdas <goodjav141592@gmail.com>
2024-12-02 12:40:42 +0900

    chore : product 엔티티 table 어노테이션 추가

--- com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/model/Product.java
+++ com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/model/Product.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,6 +16,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "p_product")
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
