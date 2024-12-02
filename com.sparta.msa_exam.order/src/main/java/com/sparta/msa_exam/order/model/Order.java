167bc7a40f82fb971ea204882aa93065de9907b6
strongcookdas <goodjav141592@gmail.com>
2024-12-02 12:36:40 +0900

    feat : order 엔티티 생성

---
+++ com.sparta.msa_exam.order/src/main/java/com/sparta/msa_exam/order/model/Order.java
@@ -0,0 +1,39 @@
+package com.sparta.msa_exam.order.model;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "p_order")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    @ElementCollection
+    @CollectionTable(name = "order_items", joinColumns = @JoinColumn(name = "order_id"))
+    @Column(name = "order_item_id")
+    private List<Long> orderItemIds;
+}
---
+++ com.sparta.msa_exam.order/src/main/java/com/sparta/msa_exam/order/model/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.sparta.msa_exam.order.model;
+
+public enum OrderStatus {
+    CREATED, PAID, SHIPPED, COMPLETED, CANCELLED
+}
